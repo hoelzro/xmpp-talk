454a4cd555fc357b91ba70af583753132c089c99
Rob Hoelz <rob@hoelz.ro>
2010-12-21 00:14:20 -0600

    Adjust the echo bot's JID

--- examples/echo-bot.pl
+++ examples/echo-bot.pl
@@ -14,7 +14,7 @@ use AnyEvent::XMPP::IM::Connection;
 my $cond = AnyEvent->condvar;
 
 my $conn = AnyEvent::XMPP::IM::Connection->new(
-    jid      => 'rob@localhost',
+    jid      => 'rob@chat.madmongers',
     password => 'abc123',
 );
 
