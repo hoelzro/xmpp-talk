5241644af41ed73708f36e80953981a98b20c6e6
Rob Hoelz <rhoelz@inoc.com>
2010-12-21 09:37:41 -0600

    Added ping.pl

---
+++ examples/ping.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use feature 'say';
+
+use AnyEvent::XMPP::IM::Connection;
+
+my ( $dest ) = @ARGV;
+
+my $cond = AnyEvent->condvar;
+my $conn = AnyEvent::XMPP::IM::Connection->new(
+    jid      => 'bot@chat.madmongers',
+    password => 'abc123',
+);
+
+$conn->reg_cb(session_ready => sub {
+    $conn->send_iq('get', sub {
+        my ( $w ) = @_;
+
+        $w->emptyTag('ping', xmlns => 'urn:xmpp:ping');
+    }, sub {
+        my ( $result, $error ) = @_;
+
+        if($error) {
+            say $error->string;
+        } else {
+            say 'received pong';
+        }
+        $cond->send;
+    }, to => 'rob@chat.madmongers');
+});
+
+$conn->connect;
+
+$cond->recv;
